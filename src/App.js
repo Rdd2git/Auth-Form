713c36b6174ac59480bd63bbe4157f92de0f59a2
Radik Galiullin <rgaliullin55@gmail.com>
2021-10-22 06:23:37 +0300

    create userName element

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 
 import './App.css';
-
+import UserName from './User/User';
+const user = {
+  login:'developer21',
+  password:123456
+}
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-       
+       <UserName name={user}/>
       </header>
     </div>
   );
---
+++ src/User/User.jsx
@@ -0,0 +1,9 @@
+function UserName({name}) {
+    return(
+        <>
+            <h1>{name.login}</h1>
+        </>
+    )
+}
+
+export default UserName;
